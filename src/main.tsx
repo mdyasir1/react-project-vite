7e5df82da33154e35e63de98045cda5ae161c2f2
mdyasir1 <mdyasir4145@gmail.com>
2024-12-03 16:54:30 +0530

    issue resolved

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Register_UI from './components/Register.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
